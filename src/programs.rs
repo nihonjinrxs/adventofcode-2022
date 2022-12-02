c1b299524fb78d6594d51bed68ba136a64fd6cd8
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-12-02 02:13:24 +0000

    Setup for day 1 part two

--- src/programs.rs
+++ src/programs.rs
@@ -1,8 +1,10 @@
 pub mod day01a;
+pub mod day01b;
 
 pub fn run_program(program_name: &str, input: &str) -> String {
     match program_name {
         "01a" | "1a" => day01a::run(&input).to_owned(),
+        "01b" | "1b" => day01b::run(&input).to_owned(),
         _ => String::from("Unknown program")
     }
 }
