3d859f08b5674fa1e1985e0fdb1e94903079ef20
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2022-12-04 05:35:32 +0000

    Refactor day 3 part one to support adding part two

--- src/programs/day03.rs
+++ src/programs/day03.rs
@@ -6,10 +6,10 @@ pub fn run(part_number: Parts, input: &str) -> String {
     format!("{}", result)
 }
 
-fn compute_result(part_number: Parts, compartments: Vec<(String, String)>) -> i32 {
+fn compute_result(part_number: Parts, rucksacks: Vec<String>) -> i32 {
     match part_number {
-        Parts::One => compute_priority_sum(compartments),
-        Parts::Two => compute_priority_sum(compartments),
+        Parts::One => compute_misplaced_priority_sum(rucksacks),
+        Parts::Two => compute_badge_priority_sum(rucksacks),
     }
 }
 
@@ -24,12 +24,17 @@ fn priority_for_char(c: char) -> i32 {
     }
 }
 
-fn compute_priority_sum(compartments: Vec<(String, String)>) -> i32 {
+fn compute_misplaced_priority_sum(rucksacks: Vec<String>) -> i32 {
+    let compartments = split_compartments(rucksacks);
     let misplaced = find_misplaced(compartments);
     misplaced.iter().map(|c| priority_for_char(*c)).sum()
 }
 
-fn get_shared(rucksack: &(String, String)) -> char {
+fn compute_badge_priority_sum(rucksacks: Vec<String>) -> i32 {
+    rucksacks.len() as i32
+}
+
+fn get_shared_in_rucksack(rucksack: &(String, String)) -> char {
     let compartment_one = rucksack.0.chars();
     let filtered = compartment_one.filter(|c| 
         rucksack.1.as_str().contains(*c)).collect::<Vec<char>>();
@@ -37,20 +42,28 @@ fn get_shared(rucksack: &(String, String)) -> char {
 }
 
 fn find_misplaced(compartments: Vec<(String, String)>) -> Vec<char> {
-    compartments.iter().map(get_shared).collect::<Vec<char>>()
+    compartments.iter().map(get_shared_in_rucksack).collect::<Vec<char>>()
 }
 
-fn collect_rucksacks(input: &str) -> Vec<(String, String)> {
-    let mut rucksacks: Vec<(String, String)> = vec![];
-    for l in input.split('\n') {
-        if l.trim().is_empty() { continue }
-        let trimmed = String::from(l.trim());
-        let half = trimmed.len() / 2;
-        let compartments = trimmed.split_at(half);
-        rucksacks.push((
+fn split_compartments(rucksacks: Vec<String>) -> Vec<(String, String)> {
+    let mut rucksack_compartments: Vec<(String, String)> = vec![];
+    rucksacks.iter().for_each(|rucksack| {
+        let half = rucksack.len() / 2;
+        let compartments = rucksack.split_at(half);
+        rucksack_compartments.push((
             String::from(compartments.0),
             String::from(compartments.1),
         ));
+    });
+    rucksack_compartments
+}
+
+fn collect_rucksacks(input: &str) -> Vec<String> {
+    let mut rucksacks: Vec<String> = vec![];
+    for l in input.split('\n') {
+        if l.trim().is_empty() { continue }
+        let trimmed = String::from(l.trim());
+        rucksacks.push(trimmed);
     }
     rucksacks
 }
@@ -59,97 +72,89 @@ fn collect_rucksacks(input: &str) -> Vec<(String, String)> {
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
-    use crate::test_helpers::vec_compare;
+    use crate::test_helpers::{
+        vec_compare,
+        TestCase,
+    };
 
     use std::fs;
+
+    fn fixture_rucksacks() -> Vec<String> {
+        vec![
+            String::from("vJrwpWtwJgWrhcsFMMfFFhFp"),
+            String::from("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"),
+            String::from("PmmdzqPrVvPwwTWBwg"),
+            String::from("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"),
+            String::from("ttgJtRGJQctTZtZT"),
+            String::from("CrZsJsPPZsGzwwsLwLmpwMDw"),
+        ]
+    }
+
+    fn fixture_compartments() -> Vec<(String, String)> {
+        vec![
+            (
+                String::from("vJrwpWtwJgWr"),
+                String::from("hcsFMMfFFhFp"),
+            ),
+            (
+                String::from("jqHRNqRjqzjGDLGL"),
+                String::from("rsFMfFZSrLrFZsSL"),
+            ),
+            (
+                String::from("PmmdzqPrV"),
+                String::from("vPwwTWBwg"),
+            ),
+            (
+                String::from("wMqvLMZHhHMvwLH"),
+                String::from("jbvcjnnSBnvTQFn"),
+            ),
+            (
+                String::from("ttgJtRGJ"),
+                String::from("QctTZtZT"),
+            ),
+            (
+                String::from("CrZsJsPPZsGz"),
+                String::from("wwsLwLmpwMDw"),
+            ),
+        ]
+    }
+
+    fn fixture_missing() -> Vec<char> {
+        vec!['p', 'L', 'P', 'v', 't', 's']
+    }
 
     #[test]
     fn test_collect_rucksacks() {
         let fixture_file = "./data/day03/test.txt";
         let test_input = fs::read_to_string(fixture_file)
             .expect("Failed to read input file");
-        let rucksacks = collect_rucksacks(&test_input);
-        let result = rucksacks.iter()
-                .map(|(a, b)| (a.as_str(), b.as_str()))
-                .collect::<Vec<(&str, &str)>>();
-        let expected = vec![
-            ("vJrwpWtwJgWr", "hcsFMMfFFhFp"),
-            ("jqHRNqRjqzjGDLGL", "rsFMfFZSrLrFZsSL"),
-            ("PmmdzqPrV", "vPwwTWBwg"),
-            ("wMqvLMZHhHMvwLH", "jbvcjnnSBnvTQFn"),
-            ("ttgJtRGJ", "QctTZtZT"),
-            ("CrZsJsPPZsGz", "wwsLwLmpwMDw"),
-        ];
+        let result = collect_rucksacks(&test_input);
+        let expected = fixture_rucksacks();
         assert!(vec_compare(&result, &expected));
     }
 
-    struct TestCase<TInput, TExpected> {
-        input: TInput,
-        expected: TExpected,
-    }
-
-    fn cases() -> Vec<TestCase<(String, String), char>> {
-        vec![
-            TestCase {
-                input: (
-                    String::from("vJrwpWtwJgWr"),
-                    String::from("hcsFMMfFFhFp"),
-                ),
-                expected: 'p',
-            },
-            TestCase {
-                input: (
-                    String::from("jqHRNqRjqzjGDLGL"),
-                    String::from("rsFMfFZSrLrFZsSL"),
-                ),
-                expected: 'L',
-            },
-            TestCase {
-                input: (
-                    String::from("PmmdzqPrV"),
-                    String::from("vPwwTWBwg"),
-                ),
-                expected: 'P',
-            },
-            TestCase {
-                input: (
-                    String::from("wMqvLMZHhHMvwLH"),
-                    String::from("jbvcjnnSBnvTQFn"),
-                ),
-                expected: 'v',
-            },
-            TestCase {
-                input: (
-                    String::from("ttgJtRGJ"),
-                    String::from("QctTZtZT"),
-                ),
-                expected: 't',
-            },
-            TestCase {
-                input: (
-                    String::from("CrZsJsPPZsGz"),
-                    String::from("wwsLwLmpwMDw"),
-                ),
-                expected: 's',
-            },
-        ]
+    #[test]
+    fn test_split_compartments() {
+        let rucksacks = fixture_rucksacks();
+        let expected = fixture_compartments();
+        let result = split_compartments(rucksacks);
+        assert!(vec_compare(&result, &expected));
     }
 
     #[test]
-    fn test_get_shared() {
-        cases().iter().for_each(|case|
-            assert_eq!(get_shared(&case.input), case.expected)
+    fn test_get_shared_in_rucksack() {
+        let inputs = fixture_compartments();
+        let expecteds = fixture_missing();
+        TestCase::create_many(inputs, expecteds).iter().for_each(|case|
+            assert_eq!(get_shared_in_rucksack(&case.input), case.expected)
         );
     }
 
     #[test]
     fn test_find_misplaced() {
-        let cases = cases();
-        let inputs = cases.iter().map(|case|
-            (case.input.0.clone(), case.input.1.clone())
-        ).collect::<Vec<(String, String)>>();
+        let inputs = fixture_compartments();
+        let expected = fixture_missing();
         let results = find_misplaced(inputs);
-        let expected = cases.iter().map(|case| case.expected).collect::<Vec<char>>();
         assert!(vec_compare(&results, &expected));
     }
 
@@ -167,12 +172,9 @@ mod tests {
     }
 
     #[test]
-    fn test_compute_priority_sum() {
-        let cases = cases();
-        let inputs = cases.iter().map(|case|
-            (case.input.0.clone(), case.input.1.clone())
-        ).collect::<Vec<(String, String)>>();
-        let result = compute_priority_sum(inputs);
+    fn test_compute_misplaced_priority_sum() {
+        let inputs = fixture_rucksacks();
+        let result = compute_misplaced_priority_sum(inputs);
         assert_eq!(result, 157);
     }
 
--- src/test_helpers.rs
+++ src/test_helpers.rs
@@ -2,3 +2,18 @@ pub fn vec_compare<T: PartialEq>(a: &Vec<T>, b: &Vec<T>) -> bool {
     let match_count = a.iter().zip(b.iter()).filter(|&(a, b)| a == b).count();
     match_count == a.len() && match_count == b.len()
 }
+
+pub struct TestCase<TInput, TExpected> {
+    pub input: TInput,
+    pub expected: TExpected,
+}
+
+impl<TInput: Clone, TExpected: Clone> TestCase<TInput, TExpected> {
+    pub fn create_many(inputs: Vec<TInput>, expecteds: Vec<TExpected>) -> Vec<TestCase<TInput, TExpected>> {
+        let pairs_iter = inputs.iter().zip(expecteds.iter());
+        pairs_iter.map(|(input, expected)| TestCase{
+            input: input.clone(),
+            expected: expected.clone(),
+        }).collect::<Vec<TestCase<TInput, TExpected>>>()
+    }
+}
